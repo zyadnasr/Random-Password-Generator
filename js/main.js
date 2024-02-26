93f24fa4ec440a081486efe75565ab8a37baf070
ZYAD NASR <156793692+zyadnasr@users.noreply.github.com>
2024-02-26 21:51:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghjklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+const allChars = upperCase + lowerCase + number + symbol;
+
+// function to generate the random pass
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+// copy function
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
